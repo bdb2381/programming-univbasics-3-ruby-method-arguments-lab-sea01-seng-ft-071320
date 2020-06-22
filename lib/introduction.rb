56e25f00ce6571199458866c60b538b6dad4219c
Brad Beggs <brad.beggs@gmail.com>
2020-06-22 20:23:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ end
 
 
 def introduction(name, language)
-  puts "Hi, my name is #{{name} and I'm learning to program in #{language}}"
+  puts "Hi, my name is #{{name} and I'm learning to program in #{language}"
 end
 
 def introduction_with_language (name, language="Ruby")
